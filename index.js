28cc5d1beab23cbc82de56e93a9a94181ceedf10
Santiago Herrera <55866725+santiherr32@users.noreply.github.com>
2024-06-08 13:45:58 -0500

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const server = require("./src/app.js");
-const { conn } = require("./src/db.js");
-
-// Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
-  });
-});
