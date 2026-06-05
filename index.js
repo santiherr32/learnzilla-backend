5f7ec29d834be518b047eb3d0bfcc44a7d219f48
Santiago Herrera <santiherr32@pm.me>
2026-06-05 18:49:03 -0500

    Refactor server start to remove model syncing
    Removed model syncing on server start.

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const server = require("../src/app.js");
 const { conn } = require("../src/db.js");
 
-// Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => {
     console.log(`%s listening at ${process.env.PORT}`);
 });
