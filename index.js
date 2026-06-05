16f5ac98f26625042ccb6dd38adde56b6485a7c2
Santiago Herrera <santiherr32@pm.me>
2026-06-05 18:15:42 -0500

    create index file for express server

---
+++ index.js
@@ -0,0 +1,8 @@
+const server = require("../src/app.js");
+const { conn } = require("../src/db.js");
+
+// Syncing all the models at once.
+conn.sync({ force: true }).then(() => {
+  server.listen(process.env.PORT, () => {
+    console.log(`%s listening at ${process.env.PORT}`);
+});
