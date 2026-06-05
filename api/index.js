16527b6e488ee737bf39d926f1caea7cb22f497c
Santiago Herrera <santiherr32@pm.me>
2026-06-05 18:28:01 -0500

    Add logging before syncing database models
    Added logging before the database sync operation.

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 const server = require("../src/app.js");
 const { conn } = require("../src/db.js");
 
+console.log("Before sync");
+
 // Syncing all the models at once.
 conn.sync({ force: true })
   .then(() => console.log("SYNC COMPLETE"))
