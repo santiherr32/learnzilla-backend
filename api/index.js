3427e1ad6109b53ff70dad853d24720395748f43
Santiago Herrera <santiherr32@pm.me>
2026-06-05 18:47:59 -0500

    Remove database sync logic from index.js
    Removed database sync logic from index.js.

--- api/index.js
+++ api/index.js
@@ -1,16 +1,3 @@
 const server = require("../src/app.js");
-const { conn } = require("../src/db.js");
-
-console.log("Before sync");
-
-(async () => {
-  try {
-    console.log("Starting sync");
-    await conn.sync({ force: true, logging: console.log});
-    console.log("SYNC COMPLETE");
-  } catch (err) {
-    console.error("SYNC ERROR", err);
-  }
-})();
 
 module.exports = server;
