2e1817ad56ad05d1068cb26623d1715b0df99c79
Santiago Herrera <santiherr32@pm.me>
2026-06-05 18:22:37 -0500

    Enhance database sync with logging
    Add logging for database sync completion and errors

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const server = require("../src/app.js");
 const { conn } = require("../src/db.js");
 
 // Syncing all the models at once.
-await conn.sync({ force: true });
+conn.sync({ force: true })
+  .then(() => console.log("SYNC COMPLETE"))
+  .catch(err => console.error("SYNC ERROR", err));
 
 module.exports = server;
