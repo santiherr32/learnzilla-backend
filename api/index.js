49f3a080e7ef0e0a3599070f1f3c08cb79b2ddcc
Santiago Herrera <santiherr32@pm.me>
2026-06-05 18:19:29 -0500

    Refactor server start to use await for sync on vercel

--- api/index.js
+++ api/index.js
@@ -1,10 +1,7 @@
 const server = require("../src/app.js");
 const { conn } = require("../src/db.js");
 
-console.log("Before sync");
-
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log(`%s listening at ${process.env.PORT}`);
-});
+await conn.sync({ force: true });
+
+module.exports = server;
